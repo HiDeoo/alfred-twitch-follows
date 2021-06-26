12093bc0b0a4e0acd51cbf85a0baf81f2bd426d7
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2021-06-26 14:18:01 +0200

    Add alfred/send tests

--- alfred/send_test.go
+++ alfred/send_test.go
@@ -8,6 +8,41 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var sendResultTests = []struct {
+	name  string
+	items []Item
+}{
+	{"ReturnItems", []Item{{"title item 1", "subtitle item 1"}, {"title item 2", "subtitle item 2"}}},
+	{"ReturnItem", []Item{{"title single item", "subtitle single item"}}},
+	{"ReturnNoItems", []Item{}},
+}
+
+func TestSendResult(t *testing.T) {
+	for _, test := range sendResultTests {
+		t.Run(test.name, func(t *testing.T) {
+			output := captureOutput(func() {
+				SendResult(test.items)
+			})
+
+			result := Result{}
+			err := json.Unmarshal([]byte(output), &result)
+
+			assert.Nil(t, err)
+
+			if len(test.items) > 0 {
+				for i, item := range test.items {
+					resultItem := result.Items[i].(map[string]interface{})
+
+					assert.Equal(t, resultItem["title"], item.Title)
+					assert.Equal(t, resultItem["subtitle"], item.SubTitle)
+				}
+			} else {
+				// TODO(HiDeoo)
+			}
+		})
+	}
+}
+
 func TestSendError(t *testing.T) {
 	errStr := "test error"
 
