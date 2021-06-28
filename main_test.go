a233fa733c8540b75843a9e62d3236ce677e70c0
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2021-06-28 18:14:01 +0200

    Add main/mapStreamsToItems tests

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/HiDeoo/alfred-twitch-follows/twitch"
@@ -14,7 +15,7 @@ func TestMapFollowsToItems(t *testing.T) {
 		followCount int
 	}{
 		{"ReturnNoFollows", 0},
-		{"ReturnMultipleFollows", 1},
+		{"ReturnSingleFollow", 1},
 		{"ReturnMultipleFollows", 2},
 	}
 
@@ -23,7 +24,7 @@ func TestMapFollowsToItems(t *testing.T) {
 			var follows = []twitch.Follow{}
 
 			for i := 0; i < test.followCount; i++ {
-				followJson := twitch.Follow{
+				followJSON := twitch.Follow{
 					FromId:     "123456789",
 					FromLogin:  "user",
 					ToId:       fmt.Sprintf("1234560%d", i),
@@ -32,7 +33,7 @@ func TestMapFollowsToItems(t *testing.T) {
 					FollowedAt: fmt.Sprintf("2020-03-01T02:23:4%d.009756Z", i),
 				}
 
-				follows = append(follows, followJson)
+				follows = append(follows, followJSON)
 			}
 
 			items := mapFollowsToItems(follows)
@@ -43,6 +44,58 @@ func TestMapFollowsToItems(t *testing.T) {
 				assert.Equal(t, follows[i].ToName, item.Title)
 				assert.Equal(t, fmt.Sprintf("https://www.twitch.tv/%s", follows[i].ToLogin), item.SubTitle)
 				assert.Equal(t, item.SubTitle, item.Arg)
+			}
+		})
+	}
+}
+
+func TestMapFollowedStreamsToItems(t *testing.T) {
+	tests := []struct {
+		name        string
+		followCount int
+	}{
+		{"ReturnNoFollowedStreams", 0},
+		{"ReturnSingleFollowedStream", 1},
+		{"ReturnMultipleFollowedStreams", 2},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var streams = []twitch.Stream{}
+
+			for i := 0; i < test.followCount; i++ {
+				streamJSON := twitch.Stream{
+					Id:           strconv.Itoa(i),
+					UserId:       fmt.Sprintf("1234560%d", i),
+					UserLogin:    fmt.Sprintf("user%d", i),
+					UserName:     fmt.Sprintf("User%d", i),
+					GameId:       fmt.Sprintf("game%d", i),
+					GameName:     fmt.Sprintf("Game%d", i),
+					Type:         "live",
+					Title:        fmt.Sprintf("Stream Title %d", i),
+					ViewerCount:  i * 100,
+					StartedAt:    fmt.Sprintf("2020-02-01T02:23:4%d.009756Z", i),
+					Language:     "en",
+					ThumbnailUrl: fmt.Sprintf("https://twitch.tv/stream/%d/thumbnail.png", i),
+					TagIds:       []string{"1122334455"},
+					IsMature:     false,
+				}
+
+				streams = append(streams, streamJSON)
+			}
+
+			items := mapStreamsToItems(streams)
+
+			assert.Equal(t, test.followCount, len(items))
+
+			for i, item := range items {
+				assert.Equal(t, streams[i].UserName, item.Title)
+				assert.Equal(
+					t,
+					fmt.Sprintf("%s - %d viewers - %s", streams[i].GameName, streams[i].ViewerCount, streams[i].Title),
+					item.SubTitle,
+				)
+				assert.Equal(t, fmt.Sprintf("https://www.twitch.tv/%s", streams[i].UserLogin), item.Arg)
 			}
 		})
 	}
